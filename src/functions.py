b7053163105ce5a390c2651f79c284b3ee8ba333
btakli <64867481+btakli@users.noreply.github.com>
2023-03-19 12:45:20 -0400

    Commited additional models and data profiling (not random forest since it's too big)

--- src/functions.py
+++ src/functions.py
@@ -2,63 +2,96 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import sklearn
+import ydata_profiling as yp
 
 
 def get_data(file_name: str) -> pd.DataFrame:
-    """Reads the data from the file and returns it as a pandas DataFrame"""
+    """Reads the data from the file and returns it as a pandas DataFrame.
+    
+    Description of the data:
+    - C_YEAR: Year of the accident
+    - C_MNTH: Month of the accident
+    - C_WDAY: Day of the week of the accident (1 = Monday, 7 = Sunday))
+    - C_HOUR: Hour of the accident (00 = midnight to 0:59, 23:00 - 23:59)
+    - C_SEV: Severity of the accident
+    - C_VEHS: Number of vehicles involved in the accident
+    - C_CONF: Configuration of the accident
+    - C_RCFG: Road configuration
+    - C_WTHR: Weather conditions
+    - C_RSUR: Road surface conditions
+    - C_RALN: Road alignment
+    - C_TRAF: Traffic control configuration
+    - V_ID: Vehicle ID
+    - V_TYPE: Type of vehicle
+    - V_YEAR: Year of vehicle
+    - P_ID: Person ID
+    - P_SEX: Sex of the person
+    - P_AGE: Age of the person
+    - P_PSN: Position of the person in the vehicle
+    - P_ISEV: Injury severity of the person
+    - P_SAFE: Safety equipment used by the person
+    - P_USER: Type of user
+    - C_CASE: Case number"""
     data = pd.read_csv(file_name, header=0)
+
     return data
 
+def profile_data(data: pd.DataFrame, output_path: str):
+    """Profiles the data and saves the report to the output path.
 
-def preprocess_data(raw_data: pd.DataFrame, verbose: bool) -> pd.DataFrame:
+    Arguments: 
+    data: The data to be profiled
+    output_path: The path to the output folder"""
+    report = yp.ProfileReport(data)
+    report.to_file(output_path)
+
+def preprocess_data(raw_data: pd.DataFrame, verbose: bool, columns_to_drop: list[str]) -> pd.DataFrame:
     """Preprocesses the data and returns it as a pandas DataFrame.
 
     Arguments:
     raw_data: The data to be preprocessed
     verbose: Whether to print the data dimensions, head and size reduction after preprocessing.
+    columns_to_drop: The columns to drop from the data. Excludes C_YEAR, C_CASE as it is automatically dropped.
 
-    Drops the C_YEAR column as all data is from 2019, as well as the following columns which are innapropriate for the analysis:
-    - C_HOUR: We are only interested in the day of the week, not the time of day
-    - C_SEV: We are only interested if an accident occured, not the severity of the accident
-    - C_VEHS: We are only interested if an accident occured, not the number of vehicles involved
-    - C_CONF: We are only interested if an accident occured, not the configuration of the accident
-    - C_RCFG: Not interested in road configuration
-    - C_RSUR: We will use the weather conditions instead, as they are more relevant and partially imply the road surface.
-    - C_RALN: We are only interested if an accident occured, not the road alignment
-    - C_TRAF: Not interested in the traffic control configuration
-    - V_ID: Not interested in the vehicle ID
-    - V_YEAR: Not interested in the vehicle year, it is too specific and may cause overfitting
-    - P_ID: Not interested in the person ID
-    - P_PSN: Not interested in the person's position in the car
-    - P_ISEV: Not interested in the person's injury severity
-    - P_SAFE: Not interested in the person's safety equipment
-    - C_CASE: Not interested in the case number, no bearing on the outcome
+    Preprocessing steps:
+    - We will drop all rows with missing/unknown values in the remaining columns of interest (i.e rows that contain U, UU, X, XX for any column of interest).
+    - We will drop the C_YEAR column as all data is from 2019.
+    - We will drop the C_CASE column as it is not needed.
+    - C_WTHR: Convert Q to 0 as it is still a weather condition, but different from the other values.
+    - P_SEX: Convert F to 0 and M to 1.
+    - C_MNTH and C_WDAY: Convert to int, now that we have dropped all rows with missing values.
+    - Normalize the data (i.e. convert all values to floats)
 
-    We will also drop all rows with missing/unknown values in the remaining columns of interest (i.e rows that contain U, UU, X, XX for any column of interest).
-
-    This leaves us with the following columns:
-    - C_MNTH: Month of the accident
-    - C_WDAY: Day of the week of the accident
-    - C_WTHR: Weather conditions of the accident. We convert Q to 0 as it is still a weather condition, but different from the other values.
-    - V_TYPE: Type of vehicle involved in the accident
-    - P_SEX: Sex of the person involved in the accident. Either F or M. We will convert this to 0 and 1 respectively.
-    - P_AGE: Age of the person involved in the accident
-    - P_USER: Type of user involved in the accident (driver, passenger, pedestrian, etc.)
     """
 
     if verbose:
-        print(f"Raw data dimensions: [{raw_data.shape[0]} x {raw_data.shape[1]}]")
+        print(
+            f"Raw data dimensions: [{raw_data.shape[0]} x {raw_data.shape[1]}]")
         print(raw_data.head())
         print()
 
     # Drop C_YEAR column as all data is from 2019
     data = raw_data.drop(columns=['C_YEAR'])
-    # Drop columns that are not needed
-    data = data.drop(columns=['C_HOUR', 'C_SEV', 'C_VEHS', 'C_CONF', 'C_RCFG', 'C_RSUR',
-                     'C_RALN', 'C_TRAF', 'V_ID', 'V_YEAR', 'P_ID', 'P_PSN', 'P_ISEV', 'P_SAFE', 'C_CASE'])
+
+    # Drop ID based columns
+    data = data.drop(columns=['C_CASE', 'P_ID', 'V_ID'])
     # Drop all rows with missing/unknown values in columns of interest
     # If row value is U, UU, X, XX, N, NN for any column drop the row (N/NN means not applicable)
-    data = data[~data.isin(['U', 'UU', 'X', 'XX', 'N', 'NN']).any(axis=1)]
+
+    # Drop columns mentioned in the columns_to_drop argument
+    # Remove columns we already dropped if they're in the array
+    columns_to_drop = [column for column in columns_to_drop if column in data.columns]
+    # List invalid columns we tried to drop
+    invalid_columns = [column for column in columns_to_drop if column not in data.columns]
+    if len(invalid_columns) > 0:
+        print(
+            f"Warning: Tried to drop invalid columns: {invalid_columns}. Ignoring these columns.")
+    
+    data = data.drop(columns=columns_to_drop)
+
+    data = data[~data.isin(['U', 'UU', 'UUUU', 'X', 'XX',
+                           'N', 'NN', 'NNN', 'NNNN', 'Q', 'QQ']).any(axis=1)]
 
     # Convert Q to 0 as it is still a weather condition, but different from the other values
     data['C_WTHR'] = data['C_WTHR'].replace('Q', '0')
@@ -66,6 +99,10 @@ def preprocess_data(raw_data: pd.DataFrame, verbose: bool) -> pd.DataFrame:
     # Convert F to 0 and M to 1 for P_SEX
     data['P_SEX'] = data['P_SEX'].replace('F', '0')
     data['P_SEX'] = data['P_SEX'].replace('M', '1')
+
+    # Convert C_MNTH and C_WDAY to int
+    data["C_MNTH"] = data["C_MNTH"].astype(int)
+    data["C_WDAY"] = data["C_WDAY"].astype(int)
 
     if verbose:
         row_count_difference, column_count_difference = get_size_difference(
@@ -92,7 +129,7 @@ def get_size_difference(old_data: pd.DataFrame, new_data: pd.DataFrame) -> tuple
     return (row_count_difference, column_count_difference)
 
 
-def extract_features_and_labels(data: pd.DataFrame, label_column: str) -> tuple[pd.DataFrame, np.ndarray]:
+def extract_features_and_labels(data: pd.DataFrame, label_column: str) -> tuple[np.ndarray, np.ndarray]:
     """Extracts the labels from the data and returns them as a numpy array. Also returns the data without the labels.
 
     Arguments:
@@ -106,4 +143,8 @@ def extract_features_and_labels(data: pd.DataFrame, label_column: str) -> tuple[
     """
     labels = data[label_column].to_numpy(dtype=np.int32)
     features = data.drop(columns=[label_column]).to_numpy(dtype=np.int32)
+
+    # One hot encode the features (i.e convert categorical data to numerical data)
+    features = sklearn.preprocessing.OneHotEncoder().fit_transform(features).toarray()
+
     return (features, labels)
--- src/models.py
+++ src/models.py
@@ -1,14 +1,21 @@
 # This file contains the code required to train, test and save all the ML models.
+import csv
 import os
-from pathlib import Path
 import pickle
+import time
+from pathlib import Path
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import sklearn
+import sklearn.ensemble
+import sklearn.model_selection
+import sklearn.neural_network
+import sklearn.svm
+import sklearn.tree
+
 import functions as f
 
-import sklearn
-import sklearn.model_selection
-import sklearn.tree
-import sklearn.neural_network
-import matplotlib.pyplot as plt
 
 def train_and_save_models(model, X_train, y_train, model_name: str, model_folder_path: str, overwrite: bool = False):
     """Trains and saves the model to the models folder.
@@ -20,17 +27,27 @@ def train_and_save_models(model, X_train, y_train, model_name: str, model_folder
     model_name: The name of the model (will be saved as {model_name}.pkl)
     model_folder_path: The path to the model folder
     overwrite: Whether to overwrite the model if it already exists in the models folder (default: False).
-    """
-    # Train model
-    model.fit(X_train, y_train)
-    # Save model
-    model_file = os.path.join(model_folder_path, f"{model_name}.pkl")
-    if os.path.exists(model_file) and not overwrite:
-        print(f"Model {model_name} already exists. Set overwrite=True to overwrite the model.")
-    else:
-        pickle.dump(model, open(model_file, "wb"))
 
-def test_model(model, X_test, y_test) -> tuple[float,float]:
+    Returns:
+    training time in seconds (-1 if model already exists and overwrite is False)
+    """
+
+    model_file = os.path.join(model_folder_path, f"{model_name}.pkl")
+    # If model already exists and overwrite is False
+    if os.path.exists(model_file) and not overwrite:
+        print(
+            f"Model {model_name} already exists. Set overwrite=True to overwrite the model.")
+        return -1
+    else:
+        # Train model
+        start_time = time.time()
+        model.fit(X_train, y_train)
+        pickle.dump(model, open(model_file, "wb"))
+        end_time = time.time()
+        return end_time - start_time
+
+
+def test_model(model, X_test, y_test) -> tuple:
     """Tests the model on the test set.
 
     Arguments:
@@ -39,12 +56,14 @@ def test_model(model, X_test, y_test) -> tuple[float,float]:
     y_test: The test labels
 
     Returns:
-    (accuracy, f1 score) tupleß
+    (accuracy, f1 score) tuple
     """
     y_pred = model.predict(X_test)
     accuracy = sklearn.metrics.accuracy_score(y_test, y_pred)
     f1 = sklearn.metrics.f1_score(y_test, y_pred, average="weighted")
+
     return (accuracy, f1)
+
 
 def load_model(model_name: str, model_folder_path: str):
     """Loads the model from the models folder.
@@ -59,34 +78,171 @@ def load_model(model_name: str, model_folder_path: str):
     model_file = os.path.join(model_folder_path, f"{model_name}.pkl")
     return pickle.load(open(model_file, "rb"))
 
-def main():
+
+def prepare_data() -> tuple:
+    """Prepares the data for training and testing.
+
+    Returns:
+    (X_train, X_test, y_train, y_test) tuple"""
     # import data from data folder
     parent_dir = Path(__file__).parents[1]
     file_path = os.path.join(parent_dir, 'data', "2019_dataset_en.csv")
     raw_data = f.get_data(file_path)
-    # preprocess data
 
-    data = f.preprocess_data(raw_data, verbose=True)
+    # profile data
+    # f.profile_data(raw_data, os.path.join(parent_dir, 'reports', 'raw_data_profile.html'))
+    # preprocess data
+    data = f.preprocess_data(raw_data, verbose=True,
+                             columns_to_drop=['C_SEV', 'P_SAFE'])
+    # profile data
+    # f.profile_data(data, os.path.join(parent_dir, 'reports', 'preprocessed_data_profile.html'))
 
     # Extract features and labels. We will use the day of the week as the label.
     X, y = f.extract_features_and_labels(data, "C_WDAY")
 
     # Split data into training and test sets, 80% training, 20% test
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=0)
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=0.2, random_state=0)
 
-    # Train and save models
-    model_folder_path = os.path.join(parent_dir, 'models')
+    return X_train, X_test, y_train, y_test
 
-    models_to_train = {"decision_tree": sklearn.tree.DecisionTreeClassifier(random_state=0), "mlp": sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(100, 100, 100, 100, 100), max_iter=1000, random_state=0)}
 
-    for model_name, model in models_to_train.items():
-        train_and_save_models(model, X_train, y_train, model_name, model_folder_path, overwrite=True)
+def train_and_test_models(X_train, X_test, y_train, y_test, models_folder_path: str, models: dict, test_results_file_path: str, overwrite_output=False, overwrite_saved_models=False):
+    """Trains and tests all the models. Stores models in specified models folder
+
+    Arguments:
+    - X_train: The training features
+    - X_test: The test features
+    - y_train: The training labels
+    - y_test: The test labels
+    - models_folder_path: The path to store the models to
+    - models: A dictionary of models to be trained and tested
+    - test_results_file_path: The path to the test results file
+    - overwrite_output: Whether to overwrite the output file if it already exist or to append to it (default: False).
+    - overwrite_saved_models: Whether to overwrite the saved models if they already exist or not (default: False)."""
+
+    training_times = {}  # Dictionary to store training times
+    for model_name, model in models.items():
+        print(f"Training model {model_name}...")
+        training_time = train_and_save_models(model, X_train, y_train,
+                                              model_name, models_folder_path, overwrite=overwrite_saved_models)
+
+        if training_time != -1:
+            training_times[model_name] = training_time
+        print("Done!")
 
     # Test models
-    for model_name, model in models_to_train.items():
-        loaded_model = load_model(model_name, model_folder_path)
+
+    # If it doesn't exist, create the output file csv with the header using pandas
+    if not os.path.exists(test_results_file_path):
+        pd.DataFrame(columns=['Model', 'Accuracy', 'F1 score', 'Training time', 'Hyperparameters']).to_csv(
+            test_results_file_path, index=False)
+
+    for model_name, model in models.items():
+        print(f"Loading model {model_name}...")
+        loaded_model = load_model(model_name, models_folder_path)
+        print(f"Testing model {model_name}...")
         accuracy, f1 = test_model(loaded_model, X_test, y_test)
-        print(f"Model {model_name} accuracy: {accuracy:.2f}, F1 score: {f1:.2f}")
+        print("Done!")
+        print(
+            f"Model {model_name} accuracy: {accuracy:.2f}, F1 score: {f1:.2f}")
+
+        # Write results to csv, replace entry if it already exists
+        df = pd.read_csv(test_results_file_path)
+
+        if model_name in df.Model.values:
+            if overwrite_output:
+                df["Accuracy"][df.Model == model_name] = accuracy
+                df["F1 score"][df.Model == model_name] = f1
+                df["Training time"][df.Model == model_name] = training_times[model_name] if model_name in training_times else "N/A"
+                df["Hyperparameters"][df.Model == model_name] = str(model.get_params())
+            else:
+                print(
+                    f"Model {model_name} already exists in test results file. Skipping...")
+        else:
+            addition = pd.DataFrame(
+                [[model_name, accuracy, f1, training_times[model_name] if model_name in training_times else "N/A", str(model.get_params())]], columns=['Model', 'Accuracy', 'F1 score', 'Training time', 'Hyperparameters'])
+            df = pd.concat([df, addition])
+
+        df.to_csv(test_results_file_path, index=False)
+
+
+def main():
+
+    # Prepare data
+    X_train, X_test, y_train, y_test = prepare_data()
+
+    parent_dir = Path(__file__).parents[1]
+
+    model_folder_path = os.path.join(parent_dir, 'models')
+
+    test_result_file = os.path.join(parent_dir, 'reports', 'test_results.csv')
+
+    models_to_train = {
+        "decision_tree": sklearn.tree.DecisionTreeClassifier(random_state=0),
+        "mlp": sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(100, 100, 100, 100, 100), max_iter=1000, random_state=0, verbose=True),
+        "random_forest": sklearn.ensemble.RandomForestClassifier(n_estimators=100, random_state=0, verbose=True, n_jobs=-1),
+        "svm": sklearn.svm.LinearSVC(random_state=0, verbose=True),
+        "mlp_sgd": sklearn.neural_network.MLPClassifier(hidden_layer_sizes=(100, 100, 100, 100, 100), max_iter=5000, random_state=0, verbose=True, solver="sgd"),
+    }
+
+    # Train and test models
+    train_and_test_models(X_train, X_test, y_train, y_test, model_folder_path,
+                          models_to_train, test_result_file, overwrite_output=False, overwrite_saved_models=False)
+
+    # Hyperparameter search:
+
+    # Hyperparameter seach dictionaries
+    mlp_hyperparameters = {
+        "hidden_layer_sizes": [(100, 100, 100), (1000, 500, 100)],
+        "max_iter": [1000],
+        "random_state": [0],
+        "verbose": [True],
+        "solver": ["adam", "sgd"],
+        "learning_rate": ["constant", "adaptive"],
+    }
+
+    decision_tree_hyperparameters = {
+        "criterion": ["gini", "entropy"],
+        "splitter": ["best", "random"],
+        "max_depth": [None, 1,  5, 10],
+        "min_samples_split": [2, 5, 10],
+        "min_samples_leaf": [1, 2, 3, 4, 5, 10],
+        "min_weight_fraction_leaf": [0.0, 0.1, 0.2, 0.5],
+        "max_features": [None, "auto", "sqrt", "log2"],
+        "random_state": [0],
+        "max_leaf_nodes": [None, 1, 2, 35, 6, 7, 8, 9, 10],
+        "min_impurity_decrease": [0.0, 0.2, 0.5]
+    }
+
+    random_forest_hyperparameters = {
+        "n_estimators": [10, 500, 1000],
+        "criterion": ["gini", "entropy"],
+        "max_depth": [None, 1, 3, 5, 10],
+        "min_samples_split": [1, 3, 5, 10],
+        "min_samples_leaf": [1, 3, 5, 10],
+        "max_features": [None, "auto", "sqrt", "log2"],
+        "n_jobs": [-1],
+        "random_state": [0],
+        "verbose": [True],
+    }
+
+    base_mlp = sklearn.neural_network.MLPClassifier(
+        random_state=0, verbose=True)
+    base_decision_tree = sklearn.tree.DecisionTreeClassifier(random_state=0)
+    base_random_forest = sklearn.ensemble.RandomForestClassifier(
+        random_state=0, verbose=True)
+
+    # Grid search uses cross validation to find the best hyperparameters, so we don't need to make a separate validation set.
+    models_to_train_search = {
+        "gridsearch_mlp": sklearn.model_selection.GridSearchCV(base_mlp, mlp_hyperparameters, cv=3, verbose=True, n_jobs=-1),
+        "gridsearch_decision_tree": sklearn.model_selection.GridSearchCV(base_decision_tree, decision_tree_hyperparameters, cv=3, verbose=True, n_jobs=-1),
+        "gridsearch_random_forest": sklearn.model_selection.GridSearchCV(base_random_forest, random_forest_hyperparameters, cv=3, verbose=True, n_jobs=-1)
+    }
+
+    train_and_test_models(X_train, X_test, y_train, y_test, model_folder_path,
+                          models_to_train_search, test_result_file, overwrite_output=False, overwrite_saved_models=False)
+
 
 if __name__ == "__main__":
     main()
