3edb341bce4627e6ac289ef806d74d47ed987535
btakli <64867481+btakli@users.noreply.github.com>
2023-03-24 18:35:49 -0400

    Moved functions to functions.py - Computed training and testing accuracy to check overfitting

--- src/models.py
+++ src/models.py
@@ -1,13 +1,7 @@
 # This file contains the code required to train, test and save all the ML models.
-import csv
 import os
-import pickle
-import time
 from pathlib import Path
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import sklearn
 import sklearn.ensemble
 import sklearn.model_selection
@@ -18,203 +12,18 @@ import sklearn.tree
 import functions as f
 
 
-def train_and_save_models(model: sklearn.base.ClassifierMixin, X_train: np.ndarray, y_train: np.ndarray, model_name: str, model_folder_path: str, overwrite: bool = False):
-    """Trains and saves the model to the models folder.
-
-    Arguments:
-    model: The model to be trained and saved
-    X_train: The training features
-    y_train: The training labels
-    model_name: The name of the model (will be saved as {model_name}.pkl)
-    model_folder_path: The path to the model folder
-    overwrite: Whether to overwrite the model if it already exists in the models folder (default: False).
-
-    Returns:
-    training time in seconds (-1 if model already exists and overwrite is False)
-    """
-
-    model_file = os.path.join(model_folder_path, f"{model_name}.pkl")
-    # If model already exists and overwrite is False
-    if os.path.exists(model_file) and not overwrite:
-        print(
-            f"Model {model_name} already exists. Set overwrite=True to overwrite the model.")
-        return -1
-    else:
-        # Train model
-        start_time = time.time()
-        model.fit(X_train, y_train)
-        pickle.dump(model, open(model_file, "wb"))
-        end_time = time.time()
-        return end_time - start_time
-
-
-def test_model(model: sklearn.base.ClassifierMixin, X_test: np.ndarray, y_test: np.ndarray) -> tuple:
-    """Tests the model on the test set.
-
-    Arguments:
-    model: The model to be tested
-    X_test: The test features
-    y_test: The test labels
-
-    Returns:
-    (accuracy, f1 score) tuple
-    """
-    y_pred = model.predict(X_test)
-    accuracy = sklearn.metrics.accuracy_score(y_test, y_pred)
-    f1 = sklearn.metrics.f1_score(y_test, y_pred, average="weighted")
-
-    return (accuracy, f1)
-
-
-def load_model(model_name: str, model_folder_path: str):
-    """Loads the model from the models folder.
-
-    Arguments:
-    model_name: The name of the model (will be loaded from {model_name}.pkl)
-    model_folder_path: The path to the model folder
-
-    Returns:
-    The model
-    """
-    model_file = os.path.join(model_folder_path, f"{model_name}.pkl")
-    return pickle.load(open(model_file, "rb"))
-
-
-def prepare_data() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
-    """Prepares the data for training and testing.
-
-    Returns:
-    (X_train, X_test, y_train, y_test) tuple"""
-    # import data from data folder
-    parent_dir = Path(__file__).parents[1]
-    file_path = os.path.join(parent_dir, 'data', "2019_dataset_en.csv")
-    raw_data = f.get_data(file_path)
-
-    # profile data
-    # f.profile_data(raw_data, os.path.join(parent_dir, 'reports', 'raw_data_profile.html'))
-    # preprocess data
-    data = f.preprocess_data(raw_data, verbose=True,
-                             columns_to_drop=['C_SEV', 'P_SAFE'])
-    # profile data
-    # f.profile_data(data, os.path.join(parent_dir, 'reports', 'preprocessed_data_profile.html'))
-
-    # Extract features and labels. We will use the day of the week as the label.
-    X, y = f.extract_features_and_labels(data, "C_WDAY")
-
-    # Split data into training and test sets, 80% training, 20% test
-    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
-        X, y, test_size=0.2, random_state=0)
-
-    return X_train, X_test, y_train, y_test
-
-
-def train_and_test_models(X_train: np.ndarray, X_test: np.ndarray, y_train: np.ndarray, y_test: np.ndarray, models_folder_path: str, models: dict, test_results_file_path: str, overwrite_output=False, overwrite_saved_models=False):
-    """Trains and tests all the models. Stores models in specified models folder
-
-    Arguments:
-    - X_train: The training features
-    - X_test: The test features
-    - y_train: The training labels
-    - y_test: The test labels
-    - models_folder_path: The path to store the models to
-    - models: A dictionary of models to be trained and tested
-    - test_results_file_path: The path to the test results file
-    - overwrite_output: Whether to overwrite the output file if it already exist or to append to it (default: False).
-    - overwrite_saved_models: Whether to overwrite the saved models if they already exist or not (default: False)."""
-
-    training_times = {}  # Dictionary to store training times
-    for model_name, model in models.items():
-        print(f"Training model {model_name}...")
-        training_time = train_and_save_models(model, X_train, y_train,
-                                              model_name, models_folder_path, overwrite=overwrite_saved_models)
-
-        if training_time != -1:
-            training_times[model_name] = training_time
-        print("Done!")
-
-    # Test models
-
-    # If it doesn't exist, create the output file csv with the header using pandas
-    if not os.path.exists(test_results_file_path):
-        pd.DataFrame(columns=['Model', 'Accuracy', 'F1 score', 'Training time', 'Hyperparameters']).to_csv(
-            test_results_file_path, index=False)
-
-    for model_name, model in models.items():
-        print(f"Loading model {model_name}...")
-        loaded_model = load_model(model_name, models_folder_path)
-        print(f"Testing model {model_name}...")
-        accuracy, f1 = test_model(loaded_model, X_test, y_test)
-        print("Done!")
-        print(
-            f"Model {model_name} accuracy: {accuracy:.2f}, F1 score: {f1:.2f}")
-
-        # Write results to csv, replace entry if it already exists
-        df = pd.read_csv(test_results_file_path)
-
-        if model_name in df.Model.values:
-            if overwrite_output:
-                df["Accuracy"][df.Model == model_name] = accuracy
-                df["F1 score"][df.Model == model_name] = f1
-                df["Training time"][df.Model ==
-                                    model_name] = training_times[model_name] if model_name in training_times else "N/A"
-                df["Hyperparameters"][df.Model ==
-                                      model_name] = str(model.get_params())
-            else:
-                print(
-                    f"Model {model_name} already exists in test results file. Skipping...")
-        else:
-            addition = pd.DataFrame(
-                [[model_name, accuracy, f1, training_times[model_name] if model_name in training_times else "N/A", str(model.get_params())]], columns=['Model', 'Accuracy', 'F1 score', 'Training time', 'Hyperparameters'])
-            df = pd.concat([df, addition])
-
-        df.to_csv(test_results_file_path, index=False)
-
-
-def plot_loss_curve(model_name: str, models_folder: str, output_folder_path: str):
-    """Plots the loss curves for the specified model.
-
-    Arguments:
-    - model_name: The name of the model to plot the loss curve for
-    - models_folder: The folder containing the models
-    - output_folder_path: The folder to store the loss curve plot to"""
-
-    model = load_model(model_name, models_folder)
-
-    if "gridsearch" in model_name:
-        print("Model is a gridsearch object. Getting best estimator...")
-        model = model.best_estimator_
-
-    if not hasattr(model, "loss_curve_"):
-        print(
-            f"Model {model_name} does not have a loss curve. Skipping plotting...")
-        return
-
-    if not os.path.exists(output_folder_path):
-        os.makedirs(output_folder_path)
-
-    # If we are using gridsearch, we need to get the best estimator, 
-    # otherwise it tries to get the curve from the gridsearch object, 
-    # which doesn't have it.
-
-    plt.plot(model.loss_curve_)
-    plt.title(f"Loss curve for model {model_name}")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.savefig(os.path.join(output_folder_path,
-                f"{model_name}_loss_curve.png"))
-    plt.clf()
-
-
 def main():
 
     # Prepare data
-    X_train, X_test, y_train, y_test = prepare_data()
+    X_train, X_test, y_train, y_test = f.prepare_data()
 
     parent_dir = Path(__file__).parents[1]
 
     model_folder_path = os.path.join(parent_dir, 'models')
 
     test_result_file = os.path.join(parent_dir, 'reports', 'test_results.csv')
+
+    training_and_testing_results_file = os.path.join(parent_dir, 'reports', 'training_and_testing_comparison_results.csv')
 
     models_to_train = {
         "decision_tree": sklearn.tree.DecisionTreeClassifier(random_state=0),
@@ -225,7 +34,7 @@ def main():
     }
 
     # Train and test models
-    train_and_test_models(X_train, X_test, y_train, y_test, model_folder_path,
+    f.train_and_test_models(X_train, X_test, y_train, y_test, model_folder_path,
                           models_to_train, test_result_file, overwrite_output=False, overwrite_saved_models=False)
 
     # Hyperparameter search:
@@ -272,18 +81,19 @@ def main():
         "gridsearch_random_forest": sklearn.model_selection.GridSearchCV(base_random_forest, random_forest_hyperparameters, cv=3, verbose=True, n_jobs=-1)
     }
 
-    train_and_test_models(X_train, X_test, y_train, y_test, model_folder_path,
+    f.train_and_test_models(X_train, X_test, y_train, y_test, model_folder_path,
                           models_to_train_search, test_result_file, overwrite_output=False, overwrite_saved_models=False)
+    
+     # Compare training and testing accuracy and f1-score for each model
+    print("Comparing training and testing accuracy and f1-score...")
+    for model_name in models_to_train | models_to_train_search:
+        f.compare_training_and_testing_accuracy(model_name, model_folder_path, X_train, X_test, y_train, y_test,
+                                                training_and_testing_results_file, overwrite_output=True)
 
     # Plot the loss curve of each model
-    for model_name in models_to_train:
-        plot_loss_curve(model_name, model_folder_path, os.path.join(
+    for model_name in models_to_train | models_to_train_search:
+        f.plot_loss_curve(model_name, model_folder_path, os.path.join(
             parent_dir, 'reports', 'figures'))
-
-    for model_name in models_to_train_search:
-        plot_loss_curve(model_name, model_folder_path, os.path.join(
-            parent_dir, 'reports', 'figures'))
-
 
 if __name__ == "__main__":
     main()
